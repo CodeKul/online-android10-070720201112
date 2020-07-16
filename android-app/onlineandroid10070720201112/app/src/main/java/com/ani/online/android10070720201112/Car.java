c9127651beb97f336827f51660f7a4d9ed40954b
aniruddha <aniruddha.kudalkar@gmail.com>
2020-07-16 12:16:54 +0530

    basic : Hello world app

---
+++ android-app/onlineandroid10070720201112/app/src/main/java/com/ani/online/android10070720201112/Car.java
@@ -0,0 +1,7 @@
+package com.ani.online.android10070720201112;
+
+public class Car {
+
+    public int type;
+    public String name;
+}
---
+++ android-app/onlineandroid10070720201112/app/src/main/java/com/ani/online/android10070720201112/starter/my.kt
@@ -0,0 +1,10 @@
+package com.ani.online.android10070720201112.starter
+
+import com.ani.online.android10070720201112.Car
+
+class MyApp {
+
+    fun installCar() {
+        val car = Car()
+    }
+}
