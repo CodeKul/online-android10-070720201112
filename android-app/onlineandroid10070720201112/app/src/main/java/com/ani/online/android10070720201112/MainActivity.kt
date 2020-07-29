42ce3238fb94bcb137dfc3abf7683258b2ec702d
aniruddha <aniruddha.kudalkar@gmail.com>
2020-07-29 12:16:17 +0530

    login : event handling

--- android-app/onlineandroid10070720201112/app/src/main/java/com/ani/online/android10070720201112/MainActivity.kt
+++ android-app/onlineandroid10070720201112/app/src/main/java/com/ani/online/android10070720201112/MainActivity.kt
@@ -3,7 +3,7 @@ package com.ani.online.android10070720201112
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +13,10 @@ class MainActivity : AppCompatActivity() {
 
         btLgn.setOnClickListener {
             it?.let {
+                val etUsNm = findViewById<EditText>(R.id.etUsNm)
                 Log.i("@ani", "Login Clicked")
+                Log.i("@ani", "User Name - ${etUsNm.text}")
+                Log.i("@ani", "Password - ${etPass.text}")
             }
         }
     }
