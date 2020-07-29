90c473f22ebf5fac5d244f2c0695baa63b43bbc2
aniruddha <aniruddha.kudalkar@gmail.com>
2020-07-29 11:39:32 +0530

    login : event handling

--- android-app/onlineandroid10070720201112/app/src/main/java/com/ani/online/android10070720201112/MainActivity.kt
+++ android-app/onlineandroid10070720201112/app/src/main/java/com/ani/online/android10070720201112/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ani.online.android10070720201112
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btLgn.setOnClickListener {
+            it?.let {
+                Log.i("@ani", "Login Clicked")
+            }
+        }
     }
 }
