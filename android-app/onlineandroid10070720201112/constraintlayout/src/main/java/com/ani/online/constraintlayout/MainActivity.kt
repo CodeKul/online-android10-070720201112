611dc6248e0fc8c7c3ff988473d842001a9af084
aniruddha <aniruddha.kudalkar@gmail.com>
2020-08-03 11:39:16 +0530

    activity : interactivity communication

--- android-app/onlineandroid10070720201112/constraintlayout/src/main/java/com/ani/online/constraintlayout/MainActivity.kt
+++ android-app/onlineandroid10070720201112/constraintlayout/src/main/java/com/ani/online/constraintlayout/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.ani.online.constraintlayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutItem.setOnClickListener {
+            val cls: Class<ChatActivity> = ChatActivity::class.java
+            val chatIntent = Intent(this@MainActivity, cls)
+            startActivity(chatIntent)
+        }
     }
 }
