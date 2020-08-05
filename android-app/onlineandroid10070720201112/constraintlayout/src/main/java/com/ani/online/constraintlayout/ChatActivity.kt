1b461616aa6d7f9f4d2885f37dd3bf1c16e80e81
aniruddha <aniruddha.kudalkar@gmail.com>
2020-08-05 15:25:26 +0530

    activity : interactivity communication - sending result back

--- android-app/onlineandroid10070720201112/constraintlayout/src/main/java/com/ani/online/constraintlayout/ChatActivity.kt
+++ android-app/onlineandroid10070720201112/constraintlayout/src/main/java/com/ani/online/constraintlayout/ChatActivity.kt
@@ -1,7 +1,11 @@
 package com.ani.online.constraintlayout
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,8 +13,23 @@ class ChatActivity : AppCompatActivity() {
         setContentView(R.layout.activity_chat)
 
        val chatBundle = intent?.extras
-        val from = chatBundle?.getString("from")
+        val from = chatBundle?.getString(MainActivity.KEY_FROM)
 
         supportActionBar?.title = from ?: "ConstraintLayout"
+
+        btSend.setOnClickListener {
+            txtHi.text = etChat.text
+        }
+
+        txtHi.setOnClickListener {
+            val dtInt = Intent()
+            dtInt.putExtra(KEY_DATA_CHAT, txtHi.text.toString())
+            setResult(Activity.RESULT_OK, dtInt)
+            finish()
+        }
+    }
+
+    companion object {
+        const val KEY_DATA_CHAT = "data_chat"
     }
 }
--- android-app/onlineandroid10070720201112/constraintlayout/src/main/java/com/ani/online/constraintlayout/MainActivity.kt
+++ android-app/onlineandroid10070720201112/constraintlayout/src/main/java/com/ani/online/constraintlayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ani.online.constraintlayout
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,12 +15,28 @@ class MainActivity : AppCompatActivity() {
         layoutItem.setOnClickListener {
 
             val chatBundle = Bundle()
-            chatBundle.putString("from", txtNm.text.toString())
+            chatBundle.putString(KEY_FROM, txtNm.text.toString())
 
             val cls: Class<ChatActivity> = ChatActivity::class.java
             val chatIntent = Intent(this@MainActivity, cls)
             chatIntent.putExtras(chatBundle)
-            startActivity(chatIntent)
+//            startActivity(chatIntent)
+            startActivityForResult(chatIntent, REQ_CHAT)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQ_CHAT) {
+            if(resultCode == Activity.RESULT_OK) {
+                msg.text = data?.getStringExtra(ChatActivity.KEY_DATA_CHAT)
+            }
+        }
+    }
+
+    companion object {
+        const val KEY_FROM = "from"
+        const val REQ_CHAT = 9879
     }
 }
