9b7ae452800ff4d19dcd9e1962fdc950a690d82c
aniruddha <aniruddha.kudalkar@gmail.com>
2020-08-12 12:27:08 +0530

    databinding : observing the live data

--- android-app/onlineandroid10070720201112/databinding/src/main/java/com/ani/online/databinding/LoginViewModel.kt
+++ android-app/onlineandroid10070720201112/databinding/src/main/java/com/ani/online/databinding/LoginViewModel.kt
@@ -9,14 +9,18 @@ class LoginViewModel : ViewModel() {
 
 //    ?private val _eml = MutableLiveData<String>("Android@gmail.com")
 //    private val _phn = MutableLiveData<String>("6546546")
+        private val _lgnSts = MutableLiveData<Boolean>(false)
 
 //    val eml : LiveData<String> get() = _eml
 //    val phn : LiveData<String> get() = _phn
 
     val eml : MutableLiveData<String> = MutableLiveData<String>("android@gmail.com")
     val phn : MutableLiveData<String> = MutableLiveData<String>("987979")
+    val lgnSts : LiveData<Boolean> get() = _lgnSts
 
     fun onLogin() {
+        this._lgnSts.value = (eml.value == "android") && (phn.value == "1234")
+
         Log.i("@ani", "Email ${eml.value} Phone ${phn.value}")
     }
 }
--- android-app/onlineandroid10070720201112/databinding/src/main/java/com/ani/online/databinding/MainActivity.kt
+++ android-app/onlineandroid10070720201112/databinding/src/main/java/com/ani/online/databinding/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.ani.online.databinding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ani.online.databinding.databinding.ActivityMainBinding
 
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     // 2. MVP
     // 3. MVVM
 
+    private val TAG = MainActivity::class.java.canonicalName
     private lateinit var os : String
     private val viewModel by lazy {
         ViewModelProvider(this)
@@ -32,5 +36,13 @@ class MainActivity : AppCompatActivity() {
             lifecycleOwner = this@MainActivity
             lgnVm = viewModel
         }
+
+        viewModel.lgnSts.observe(this, Observer<Boolean> {
+                sts -> if(sts) startChatActivity()
+        })
+    }
+
+    private fun startChatActivity() {
+        startActivity(Intent(this, ChatActivity::class.java))
     }
 }
