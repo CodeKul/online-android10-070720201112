a23f1bed6e34b9bb9d09aa04dd306562e0fb69c8
aniruddha <aniruddha.kudalkar@gmail.com>
2020-08-28 14:59:33 +0530

    broadcast receivers with animation

---
+++ android-app/onlineandroid10070720201112/broadcastreceivers/src/main/java/com/ani/online/broadcastreceivers/BootReceiver.kt
@@ -0,0 +1,11 @@
+package com.ani.online.broadcastreceivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        // what to do after booting
+    }
+}
--- android-app/onlineandroid10070720201112/broadcastreceivers/src/main/java/com/ani/online/broadcastreceivers/MainActivity.kt
+++ android-app/onlineandroid10070720201112/broadcastreceivers/src/main/java/com/ani/online/broadcastreceivers/MainActivity.kt
@@ -1,21 +1,52 @@
 package com.ani.online.broadcastreceivers
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val br = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.i("@ani", "${intent?.action}")
+            if(intent?.action == Intent.ACTION_POWER_CONNECTED) {
+                imgBat.setImageResource(R.drawable.ic_baseline_battery_charging_full_24)
+                val anim = AnimationUtils.loadAnimation(context, R.anim.rotate )
+                imgBat.startAnimation(anim)
+            }
+            if(intent?.action == Intent.ACTION_POWER_DISCONNECTED) {
+                imgBat.setImageResource(R.drawable.ic_baseline_battery_std_24)
+                val anim = AnimationUtils.loadAnimation(context, R.anim.rotate )
+                imgBat.startAnimation(anim)
+            }
+         }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            val samp = Intent()
-        samp.action = Intent.ACTION_CALL
-        samp.action = Intent.ACTION_DIAL
-            samp.action = Intent.ACTION_VIEW
-            samp.setData(Uri.parse("https://google.com"))
-            startActivity(samp)
+        imgBat.setOnClickListener {
+            val anim = AnimationUtils.loadAnimation(this, R.anim.rotate )
+            imgBat.startAnimation(anim)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        registerReceiver(br, filter)
+    }
+
+    override fun onStop() {
+        unregisterReceiver(br)
+        super.onStop()
     }
 }
