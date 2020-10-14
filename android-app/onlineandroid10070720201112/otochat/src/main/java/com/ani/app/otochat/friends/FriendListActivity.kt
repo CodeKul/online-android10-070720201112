89464a941ed57be6f2c1c198f4a478c45605e8c2
aniruddha <aniruddha.kudalkar@gmail.com>
2020-10-14 19:21:56 +0530

    project : OneToOne Chat - Firebase Friend List - Design

--- android-app/onlineandroid10070720201112/otochat/src/main/java/com/ani/app/otochat/friends/FriendListActivity.kt
+++ android-app/onlineandroid10070720201112/otochat/src/main/java/com/ani/app/otochat/friends/FriendListActivity.kt
@@ -2,11 +2,30 @@ package com.ani.app.otochat.friends
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import com.ani.app.otochat.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.activity_friend_list.*
 
 class FriendListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend_list)
+
+        fabAdd.setOnClickListener {
+            addFriend()
+        }
+    }
+
+    fun addFriend() {
+        val builder = MaterialAlertDialogBuilder(this, R.style.MaterialAlertDialog_rounded)
+        val alert = builder.create()
+
+        val view = LayoutInflater
+            .from(this)
+            .inflate(R.layout.dialog_add_friend, null)
+        alert.setView(view)
+        alert.show()
     }
 }
